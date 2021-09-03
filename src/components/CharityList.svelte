48f053b886e87b20e1e8ec1bdd8cbc101f9d7682
kikiAgustin <kikiagustin62@gmail.com>
2021-09-03 06:57:46 +0600

    penataan folder

---
+++ src/components/CharityList.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let charities;
+</script>
+
+<h2>Charity List</h2>
+<prev>{charities}</prev>
+<ul>
+    <li>Charity 1</li>
+    <li>Charity 2</li>
+</ul>
---
+++ src/pages/Home.svelte
@@ -0,0 +1,27 @@
+<script>
+    import CharityList from "../components/CharityList.svelte";
+
+    let title = "Charity";
+    setTimeout(function () {
+        title = "Donasi";
+    }, 2000);
+
+    let charities = ["Charity 1", "Charity 2", "Charity 3"];
+</script>
+
+<svelte:head>
+    <link
+        rel="stylesheet"
+        href="https://cdn.rawgit.com/kimeiga/bahunya/css/bahunya-0.1.3.css"
+    />
+</svelte:head>
+
+<h1>{title}</h1>
+<CharityList {charities} />
+
+<style>
+    h1 {
+        font-size: 3em;
+        color: brown;
+    }
+</style>
